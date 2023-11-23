04918c775a257442c6132441104ab0debd0462d7
MANUELBSFILHO <mbsfilho.engenharia@gmail.com>
2023-11-23 00:12:29 -0300

    primeiro commit

---
+++ SolRH/src/main/java/com/SolRH/SolRH/SolRhApplication.java
@@ -0,0 +1,13 @@
+package com.SolRH.SolRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolRhApplication.class, args);
+	}
+
+}
---
+++ SolRH/src/test/java/com/SolRH/SolRH/SolRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SolRH.SolRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
